9fdc15457777379126c63068631a958defdca8d3
m256s <61662871+m256s@users.noreply.github.com>
2020-04-02 00:04:33 +0300

    update on sign in, sign up and landing page

--- UI/script/script.js
+++ UI/script/script.js
@@ -40,6 +40,47 @@ const myArray =[];
         myArray.push(document.getElementById("names").value);
         myArray.push(document.getElementById("emailid").value);
         myArray.push(document.getElementById("password").value);
-    
+    disp();
+    createAccount();
     }
+function disp() {
+  window.location = "dashboard.html";
+  alert("Successfully signed up");
+}
 
+function myFilter() {
+        document.getElementById("Dropdown").classList.toggle("show");
+      }
+      
+      function filterFunction() {
+        var input, filter, ul, li, a, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        div = document.getElementById("Dropdown");
+        a = div.getElementsByTagName("a");
+        for (i = 0; i < a.length; i++) {
+          txtValue = a[i].textContent || a[i].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+          } else {
+            a[i].style.display = "none";
+          }
+        }
+      }
+ function signIn() {
+        let userEmail = document.getElementById("email").value;
+        let userPassword = document.getElementById("password").value;
+    
+        let email = ["sam@gmail.com","munanura@gmail.com"];
+        let paswrd = ["hhh", "ggg"];
+    
+        for(let i = 0; i < email.length; i++) {
+            if (userEmail == email[i] && userPassword == paswrd[i]) {
+                alert("Successfully logged in");
+                window.location = "signed in user.html";
+                return false;
+            } else {
+                alert("Invalid email or password");
+            }
+        }
+    }
