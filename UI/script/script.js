3d32ea8a4fa166819f296685d399976d09e6a003
ShNorah <aksharon018@gmail.com>
2020-04-01 09:00:51 +0300

    ft(signup): add validations

--- UI/script/script.js
+++ UI/script/script.js
@@ -1,39 +1,45 @@
 //create account function
 function createAccount(){
 var uname = document.getElementById("names").value;
-var uemail = document.getElementById("emailid").value;
+var uemail = document.getElementById("emailid").value;  
 var upassword = document.getElementById("password").value;
 var ucpassword = document.getElementById("cpassword").value;
 
-//validations for useremail, username and password
-var password_expression = /^(?=.*[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
-var letters=/^[A-Za-z]+$/;
-var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//validations for useremail
 
-//conditions to check the validations
-if (uname=""){
-    alert("please enter your fullnames");
-}else if(!letters.test(uname)){
-    alert("please enter on characters");
-
-}else if(uemail = ""){
-    alert("please enter your email address");
-}else if(!filter.test(uemail)){
-    alert("your email is invalid")
-}else if (upassword =""){
-    alert("please enter your password")
-}else if(!password_expression.test(upassword)){
-    alert("upper case, lower case, special characters and numeric numbers are required in this fileld");
-}else if(upassword!=ucpassword){
-    alert("the passwords do not match");
-}else if(document.getElementById("cpassword").value.length < 6){
-    alert("a minimum length of 6 is required");
-}else if (document.getElementById("cpassword").value.length > 12){
-    alert("a maximum length of 12 is required")
-}else {
-    alert("create account successful");
+function validateEmail(){  
+    let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!validEmail.test(uemail)){
+        alert("you have entered an invalid email");
+    }
 }
-window.location =" https://shnorah.github.io/Online-Shopping-Project/";
+//validate userpaname
+function validateUsername(){
+    let letters = /^[A-Za-z]+$/;
+    if (!letters.test(uname)){
+         alert("enter alphabet characters only");
+    
+}
+}
+//validate user password
+function validatePassword(){
+    let passs =/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/;
+    if(!passs.test(upassword)){
+        alert("upper case, lower case and a special character is required");
+    }else if (upassword!=ucpassword){
+        alert("passwords dont match");
+    }else{
+        alert("create account successful");
+    window.location.href ="/index.html";
+    }
+}
 
-} 
+}
+const myArray =[];
+    function addTo(){
+        myArray.push(document.getElementById("names").value);
+        myArray.push(document.getElementById("emailid").value);
+        myArray.push(document.getElementById("password").value);
+    
+    }
 
