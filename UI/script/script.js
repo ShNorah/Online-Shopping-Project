3c50ab7a66c6c55c313b19509965fe502e6d6e99
m256s <61662871+m256s@users.noreply.github.com>
2020-04-06 14:28:16 +0300

    with changes

--- UI/script/script.js
+++ UI/script/script.js
@@ -21,7 +21,7 @@ function signIn() {
       let userEmail = document.getElementById("email").value;
       let userPassword = document.getElementById("password").value;
   
-      let email = ["sam@gmail.com","munanura@gmail.com"];
+      let email = ["sam@gmail.com","sharon@gmail.com"];
       let paswrd = ["hhhhhh", "gggggg"];
   
       for(let i = 0; i < email.length; i++) {
@@ -34,82 +34,103 @@ function signIn() {
           }
       }
   }
-jQuery(document).ready(function(){
-  $('.slideshow-container').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
 
-      dots: true,
-      speed: 300,
-  });
+  jQuery(document).ready(function(){
+    $('.slideshow-container').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        speed: 300,
+    });
 
-      var dialog, form,
-   
-        // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-        emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-        name = $( "#name" ),
-        email = $( "#email" ),
-        phonenumber = $( "#phonenumber" ),
-        allFields = $( [] ).add( name ).add( email ).add( phonenumber ),
-        tips = $( ".validateTips" );
-   
-      function updateTips( t ) {
-        tips
-          .text( t )
-          .addClass( "ui-state-highlight" );
-        setTimeout(function() {
-          tips.removeClass( "ui-state-highlight", 1500 );
-        }, 500 );
-      }
-   
-      function checkRegexp( o, regexp, n ) {
-        if ( !( regexp.test( o.val() ) ) ) {
-          o.addClass( "ui-state-error" );
-          updateTips( n );
-          return false;
-        } else {
-          return true;
+        var dialog, form,
+     
+          // validating user input
+          emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+          name = $( "#name" ),
+          email = $( "#email" ),
+          phonenumber = $( "#phonenumber" ),
+          allFields = $( [] ).add( name ).add( email ).add( phonenumber ),
+          tips = $( ".validateTips" );
+     
+        function updateTips( t ) {
+          tips
+            .text( t )
+            .addClass( "ui-state-highlight" );
+          setTimeout(function() {
+            tips.removeClass( "ui-state-highlight", 1500 );
+          }, 500 );
         }
-      }
-   
-      function orderNow() {
-        var valid = true;
-        allFields.removeClass( "ui-state-error" );      
-          console.log('yes');
-          dialog.dialog({
-              close: function() { 
-                  console.log('no');
-                  window.location.href = "UI/pages/thankyou.html"; }
-          });
-      }
-   
-      dialog = $( "#dialog-form" ).dialog({
-        autoOpen: false,
-        height: 400,
-        width: 350,
-        modal: true,
-        buttons: {
-          "Make Order": orderNow,
-          Cancel: function() {
-            dialog.dialog( "close" );
+     
+        function checkRegexp( o, regexp, n ) {
+          if ( !( regexp.test( o.val() ) ) ) {
+            o.addClass( "ui-state-error" );
+            updateTips( n );
+            return false;
+          } else {
+            return true;
           }
-        },
-        close: function() {
-          form[ 0 ].reset();
-          allFields.removeClass( "ui-state-error" );
         }
-      });
-   
-      form = dialog.find( "form" ).on( "submit", function( event ) {
-        event.preventDefault();
-        orderNow();
-      });
-   
-      $( "#popup" ).button().on( "click", function() {
-        dialog.dialog( "open" );
-      });
+     
+        function orderNow() {
+          var valid = true;
+          allFields.removeClass( "ui-state-error" );      
+            console.log('yes');
+            alert("thank you, your order has been initiated, click ok to proceed");
+            window.location.href = "/UI/pages/order.html";
+            dialog.dialog({
+                close: function() { 
+                    console.log('no');                    
+                   }
+            });
+        }
+     
+        dialog = $( "#dialog-form" ).dialog({
+          autoOpen: false,
+          height: 400,
+          width: 350,
+          modal: true,
+          buttons: {
+            "Make Order": orderNow,
+            Cancel: function() {
+              dialog.dialog( "close" );
+              alert("sorry, your order has been cancelled");
+            }
+          },
+          close: function() {
+            form[ 0 ].reset();
+            allFields.removeClass( "ui-state-error" );
+          }
+        });
+     
+        form = dialog.find( "form" ).on( "submit", function( event ) {
+          event.preventDefault();
+          orderNow();
+        });
+     
+        $( "#popup" ).button().on( "click", function() {
+          dialog.dialog( "open" );
+        });
 
 });
+
+//searching an item
+function myfunction(){
+  var input, myProduct, filter, texttValue, products;
+  filter = input.value;
+  input =document.getElementById("product");
+  myProduct =document.getElementById("myProduct");
+  for (i=0; i<myProduct.lenghth; i++){
+    products = myProduct[i].detElementsByClassName("edu-product__image")[0];
+    texttValue = products.textContent || products.innerText;
+    if(texttValue.indexOf(filter)> -1){
+    myProduct[i].style.display ="";
+    }else{
+      myProduct[i].style.display = none;
+    }
+  }
+}
+
 
 //searching an item
 document.addEventListener( "DOMContent Loaded", function(){
