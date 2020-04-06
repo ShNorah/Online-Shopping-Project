34968b4560dc738419281a86a3c2b3c71586df6f
Imokol Faith Ruth <imokol@kanzucode.com>
2020-04-07 00:16:41 +0300

    Cleanups in site styles

--- UI/script/script.js
+++ UI/script/script.js
@@ -77,7 +77,7 @@ function signIn() {
           allFields.removeClass( "ui-state-error" );      
             console.log('yes');
             alert("thank you, your order has been initiated, click ok to proceed");
-            window.location.href = "/UI/pages/order.html";
+            window.location.href = "./UI/pages/order.html";
             dialog.dialog({
                 close: function() { 
                     console.log('no');                    
@@ -112,86 +112,24 @@ function signIn() {
           dialog.dialog( "open" );
         });
 
-});
-
-jQuery(document).ready(function(){
-  $('.slideshow-container').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      dots: true,
-      speed: 300,
-  });
-
-      var dialog, form,
-   
-        // validating user input
-        emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-        name = $( "#name" ),
-        email = $( "#email" ),
-        phonenumber = $( "#phonenumber" ),
-        allFields = $( [] ).add( name ).add( email ).add( phonenumber ),
-        tips = $( ".validateTips" );
-   
-      function updateTips( t ) {
-        tips
-          .text( t )
-          .addClass( "ui-state-highlight" );
-        setTimeout(function() {
-          tips.removeClass( "ui-state-highlight", 1500 );
-        }, 500 );
-      }
-   
-      function checkRegexp( o, regexp, n ) {
-        if ( !( regexp.test( o.val() ) ) ) {
-          o.addClass( "ui-state-error" );
-          updateTips( n );
-          return false;
-        } else {
-          return true;
-        }
-      }
-   
-      function orderNow() {
-        var valid = true;
-        allFields.removeClass( "ui-state-error" );      
-          console.log('yes');
-          alert("thank you, your order has been initiated, click ok to proceed");
-          window.location.href = "/UI/pages/order.html";
-          dialog.dialog({
-              close: function() { 
-                  console.log('no');                    
-                 }
-          });
-      }
-   
-      dialog = $( "#dialog-form" ).dialog({
-        autoOpen: false,
-        height: 400,
-        width: 350,
-        modal: true,
-        buttons: {
-          "Make Order": orderNow,
-          Cancel: function() {
-            dialog.dialog( "close" );
-            alert("sorry, your order has been cancelled");
-          }
-        },
-        close: function() {
-          form[ 0 ].reset();
-          allFields.removeClass( "ui-state-error" );
-        }
-      });
-   
-      form = dialog.find( "form" ).on( "submit", function( event ) {
-        event.preventDefault();
-        orderNow();
-      });
-   
-      $( "#popup" ).button().on( "click", function() {
-        dialog.dialog( "open" );
-      });
+        $('.slider-for').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          fade: true,
+          asNavFor: '.slider-nav'
+        });
+        $('.slider-nav').slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          asNavFor: '.slider-for',
+          dots: true,
+          centerMode: true,
+          focusOnSelect: true
+        });
 
 });
+
 
 //searching an item
 function myfunction(){
